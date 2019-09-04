31661e7667f65a46f7ff52c16651e2a5b94c25d3
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-04 14:50:36 +0300

    Integrate with "most"

--- library/Canapi/Api.hs
+++ library/Canapi/Api.hs
@@ -11,6 +11,9 @@ import qualified Canapi.Strelka.RequestBodyParsing as RequestBodyParsing
 import qualified Canapi.Strelka.ResponseBodyBuilding as ResponseBodyBuilding
 import qualified Data.Serialize.Get as CerealGet
 import qualified Data.Serialize.Put as CerealPut
+import qualified Most.Cereal.Get as CerealGet
+import qualified Most.Cereal.Put as CerealPut
+import qualified Most.Types as MostTypes
 
 
 binary ::
@@ -31,3 +34,26 @@ binary decoder encoder envProj errProj action = do
       ResponseBuilding.contentTypeHeader "application/octet-stream" <>
       ResponseBuilding.body (ResponseBodyBuilding.serialize (encoder response))
     )
+
+{-|
+Versioned binary endpoint.
+-}
+most ::
+  Word32 ->
+  CerealGet.Get request ->
+  (response -> CerealPut.Put) ->
+  (env -> actionEnv) ->
+  (err -> Text) ->
+  (request -> Action actionEnv err response) ->
+  Api env
+most version decoder encoder envProj errProj action = 
+  binary
+    (CerealGet.versionedRequestDecoding version decoder)
+    (CerealPut.versionedResponse version encoder)
+    (envProj)
+    (errProj)
+    (\ case
+      MostTypes.SupportedVersionedRequestDecoding request ->
+        fmap MostTypes.SupportedVersionedResponse (action request)
+      MostTypes.UnsupportedVersionedRequestDecoding _ ->
+        pure MostTypes.UnsupportedVersionedResponse)
