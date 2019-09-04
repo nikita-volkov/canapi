742098b275108364cda48e1637e3e6bc09e7de75
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-04 13:46:28 +0300

    Clean up

--- library/Canapi/Strelka/Parser.hs
+++ library/Canapi/Strelka/Parser.hs
@@ -3,11 +3,7 @@ module Canapi.Strelka.Parser where
 import Canapi.Prelude
 import Canapi.Strelka.Types
 import qualified Strelka.RequestParsing as RequestParsing
-import qualified Strelka.ResponseBuilding as ResponseBuilding
 import qualified Strelka.RequestBodyParsing as RequestBodyParsing
-import qualified Strelka.ResponseBodyBuilding as ResponseBodyBuilding
-import qualified Strelka.WAI as Wai
-import qualified Optima
 
 
 action :: (env -> actionEnv) -> (error -> Text) -> Action actionEnv error result -> Parser env result
